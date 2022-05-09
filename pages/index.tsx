4151a3df87bc7fd73fa3c30608450957ea586c2e
Alexandr <reactb0y1@gmail.com>
2022-05-09 12:39:58 +0300

    styled cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,43 @@
 import type {NextPage} from 'next'
 import styles from '../styles/Home.module.css'
 import {useGetProductsQuery} from "../store/product/product.api";
+import {useActions} from "../hooks/useActions";
 
 const Home: NextPage = () => {
     const {data, isLoading, error} = useGetProductsQuery(6);
+    const {addItem} = useActions();
 
     return (
         <div className={styles.container}>
 
-            {data && <p>data</p>}
-            {isLoading && <p>Loading</p>}
-            {error && <p>error</p>}
+            {isLoading && <p>Loading ...</p>}
+
+            {data &&
+            <div style={{display: 'grid', gridTemplateColumns: 'repeat( 2, minmax(0, 1fr) )', gap: 20}}>
+                {
+                    data.map(product =>
+                        <div key={product.id} className={'item'} style={{
+                            height: '400px',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            gap: '30px',
+                            border: '1px solid black'
+                        }}>
+                            <h3>{product.title}</h3>
+                            <img src={product.image} alt="" style={{
+                                maxWidth: '100%',
+                                maxHeight: '200px'
+                            }}/>
+                            <button onClick={() => addItem(product)}>
+                                Add to cart
+                            </button>
+                        </div>
+                    )
+                }
+            </div>
+            }
+
 
         </div>
     )
