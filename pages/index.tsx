881247a2412737634fc69adc7fe7130e27d66406
Alexandr <reactb0y1@gmail.com>
2022-05-09 11:56:19 +0300

    Home: use data in component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,19 @@
 import type {NextPage} from 'next'
 import styles from '../styles/Home.module.css'
+import {useGetProductsQuery} from "../store/product/product.api";
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
+    const {data, isLoading, error} = useGetProductsQuery(6);
 
-    </div>
-  )
+    return (
+        <div className={styles.container}>
+
+            {data && <p>data</p>}
+            {isLoading && <p>Loading</p>}
+            {error && <p>error</p>}
+
+        </div>
+    )
 }
 
 export default Home
